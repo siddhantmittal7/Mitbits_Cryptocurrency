9a0fe1a13b8d09dfb4397fef562ef3192307d993
prafful13 <prafful.mehrotra@gmail.com>
2018-11-25 22:57:52 -0500

    mining, wallet, txns, blockchain implemented

--- lib/mitbits/Miner.ex
+++ lib/mitbits/Miner.ex
@@ -1,6 +1,7 @@
 defmodule Mitbits.Miner do
   use GenServer, restart: :transient
   @target "0000" <> "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
+  # @target "00" <> "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
 
   # API
   def start_link({pk, sk, hash_name}) do
@@ -21,73 +22,113 @@ defmodule Mitbits.Miner do
   def handle_call({:mine_first, string}, _from, {pk, sk}) do
     {:ok, signature} = RsaEx.sign(string, sk)
     # {:ok, valid} = RsaEx.verify(string, signature, pk
-    [{_, curr_unchained_txns}] = :ets.lookup(:mitbits, "unchained_txn")
+    # [{_, curr_unchained_txns}] = :ets.lookup(:mitbits, "unchained_txn")
 
-    updated_unchained_txns =
-      curr_unchained_txns ++
-        [%{signature: signature, message: string, timestamp: System.system_time()}]
+    updated_unchained_txns = %{signature: signature, message: string, timestamp: System.system_time()}
 
-    :ets.insert(:mitbits, {"unchained_txn", updated_unchained_txns})
+    # :ets.insert(:mitbits, {"unchained_txn", updated_unchained_txns})
 
-    {:reply, {mine_first(pk, sk)}, {pk, sk}}
+    {:reply, {mine_first(pk, sk, updated_unchained_txns)}, {pk, sk}}
   end
 
   def handle_cast(:mine, {pk, sk}) do
-    [{_, curr_unchained_txns}] = :ets.lookup(:mitbits, "unchained_txn")
+    # [{_, curr_unchained_txns}] = :ets.lookup(:mitbits, "unchained_txn")
+    my_hash = Mitbits.Utility.getHash(pk)
+
+    {curr_unchained_txns} =
+      GenServer.call(Mitbits.Utility.string_to_atom("node_" <> my_hash), :get_txns)
 
     sorted_unchained_txns = Enum.sort_by(curr_unchained_txns, fn txn -> txn.timestamp end)
+    # IO.inspect(["lol", Enum.count(sorted_unchained_txns)])
+    # Mitbits.Utility.print_txns()
+    size_of_txn_set = 5
 
-    if Enum.count(sorted_unchained_txns) < 5 do
+    if Enum.count(sorted_unchained_txns) < size_of_txn_set do
+      # Process.sleep(1000)
       GenServer.cast(self(), :mine)
       {:noreply, {pk, sk}}
     else
-      {txn_set, remaining_unchained_txns} = Enum.split(sorted_unchained_txns, 5)
-      str_txn_set = Mitbits.Utility.combine(txn_set)
-
       my_hash = Mitbits.Utility.getHash(pk)
+      {txn_set, remaining_unchained_txns} = Enum.split(sorted_unchained_txns, size_of_txn_set)
 
-      {prev_block_hash} =
-        GenServer.call(Mitbits.Utility.string_to_atom("node_" <> my_hash), :get_prev_block_hash)
+      reward_msg = %{from: "miner_" <> my_hash, to: "node_" <> my_hash, amount: 1000}
 
-      block_string = str_txn_set <> prev_block_hash
-      nonce = Enum.random(1..100)
+      str_reward_msg =
+        to_string(reward_msg.from) <> to_string(reward_msg.to) <> to_string(reward_msg.amount)
 
-      new_block_hash = find_block_hash(block_string, nonce)
+      {:ok, signature_of_reward_txn} = RsaEx.sign(str_reward_msg, sk)
 
-      IO.inspect(new_block_hash)
+      str_signature_of_reward_txn =
+        signature_of_reward_txn |> Base.encode16() |> String.downcase()
 
-      block = %{
-        hash: new_block_hash,
-        txns: txn_set,
-        previous_hash: prev_block_hash,
+      reward_txn = %{
+        signature: signature_of_reward_txn,
+        message: reward_msg,
         timestamp: System.system_time()
       }
 
-      # IO.inspect block
+      txn_set = txn_set ++ [reward_txn]
 
-      # Delete txn_set from ets
-      :ets.insert(:mitbits, {"unchained_txn", remaining_unchained_txns})
+      str_txn_set = Mitbits.Utility.combine(txn_set)
 
-      # Send block to all
-      [{_, all_nodes}] = :ets.lookup(:mitbits, "nodes")
+      [{_, prev_block_hash}] = :ets.lookup(:mitbits, "prev_block_hash")
+      block_string = str_txn_set <> prev_block_hash
+      nonce = Enum.random(1..100_000)
 
-      Enum.each(all_nodes, fn {hash} ->
-        IO.inspect(hash)
-        {:ok} = GenServer.call(Mitbits.Utility.string_to_atom("node_" <> hash), {:rec_new_block, block})
-        {:ok} = GenServer.call(Mitbits.Utility.string_to_atom("node_" <> hash), :update_wallet)
-      end)
+      new_block_hash = find_block_hash(block_string, nonce, prev_block_hash, my_hash)
 
-      GenServer.cast(self(), :mine)
-      {:noreply, {pk, sk}}
+      if(new_block_hash == :restart) do
+        GenServer.cast(self(), :mine)
+        {:noreply, {pk, sk}}
+      else
+        # IO.inspect(new_block_hash)
+        :ets.insert(:mitbits, {"prev_block_hash", new_block_hash})
+
+        block = %{
+          hash: new_block_hash,
+          txns: txn_set,
+          previous_hash: prev_block_hash,
+          timestamp: System.system_time()
+        }
+
+        # IO.inspect block
+
+        # Delete txn_set from ets
+        # :ets.insert(:mitbits, {"unchained_txn", remaining_unchained_txns})
+        {:ok} =
+          GenServer.call(
+            Mitbits.Utility.string_to_atom("node_" <> my_hash),
+            {:delete_txns, txn_set}
+          )
+
+        # Send block to all
+        [{_, all_nodes}] = :ets.lookup(:mitbits, "nodes")
+
+        Enum.each(all_nodes, fn {hash} ->
+          IO.inspect(hash)
+
+          {:ok} =
+            GenServer.call(
+              Mitbits.Utility.string_to_atom("node_" <> hash),
+              {:rec_new_block, block}
+            )
+
+          {:ok} = GenServer.call(Mitbits.Utility.string_to_atom("node_" <> hash), :update_wallet)
+        end)
+
+        GenServer.cast(self(), :mine)
+        {:noreply, {pk, sk}}
+      end
     end
   end
 
-  def mine_first(pk, sk) do
-    [{_, curr_unchained_txns}] = :ets.lookup(:mitbits, "unchained_txn")
-    :ets.insert(:mitbits, {"unchained_txn", []})
-    sorted_unchained_txns = Enum.sort_by(curr_unchained_txns, fn txn -> txn.timestamp end)
+  def mine_first(pk, sk, first_txn) do
+    # [{_, curr_unchained_txns}] = :ets.lookup(:mitbits, "unchained_txn")
+    # :ets.insert(:mitbits, {"unchained_txn", []})
+    # sorted_unchained_txns = Enum.sort_by(curr_unchained_txns, fn txn -> txn.timestamp end)
 
-    [first_txn | _] = sorted_unchained_txns
+    # [first_txn | _] = sorted_unchained_txns
+
     str_signature_of_first_txn = first_txn.signature |> Base.encode16() |> String.downcase()
 
     str_first_txn =
@@ -115,8 +156,8 @@ defmodule Mitbits.Miner do
 
     nonce = Enum.random(1..100)
 
-    new_block_hash = find_block_hash(hash_of_first_and_reward_txn, nonce)
-
+    new_block_hash = find_first_block_hash(hash_of_first_and_reward_txn, nonce)
+    IO.inspect new_block_hash
     block = %{
       hash: new_block_hash,
       txns: [first_txn, reward_txn],
@@ -127,14 +168,32 @@ defmodule Mitbits.Miner do
     block
   end
 
-  def find_block_hash(string, nonce) do
+  def find_first_block_hash(string, nonce) do
     temp_str = string <> to_string(nonce)
     temp_hash = :crypto.hash(:sha256, temp_str) |> Base.encode16() |> String.downcase()
 
     if(temp_hash < @target) do
       temp_hash
     else
-      find_block_hash(string, nonce + 1)
+      find_first_block_hash(string, nonce + 1)
+    end
+  end
+
+  def find_block_hash(string, nonce, prev_block_hash, my_hash) do
+    temp_str = string <> to_string(nonce)
+    temp_hash = :crypto.hash(:sha256, temp_str) |> Base.encode16() |> String.downcase()
+
+    [{_, temp_prev_block_hash}] = :ets.lookup(:mitbits, "prev_block_hash")
+    # IO.puts nonce
+
+    if(temp_prev_block_hash == prev_block_hash) do
+      if(temp_hash < @target) do
+        temp_hash
+      else
+        find_block_hash(string, nonce + 1, prev_block_hash, my_hash)
+      end
+    else
+      :restart
     end
   end
 end
--- lib/mitbits/Node.ex
+++ lib/mitbits/Node.ex
@@ -14,14 +14,15 @@ defmodule Mitbits.Node do
 
   # Server
   def init({pk, sk, genesis_block}) do
-    {:ok, {pk, sk, [genesis_block], 0}}
+    {:ok, {pk, sk, [genesis_block], [], 0}}
   end
 
-  def handle_call(:get_balance, _from, {pk, sk, blockchain, balance}) do
-    {:reply, balance, {pk, sk, blockchain, balance}}
+  def handle_call(:get_balance, _from, {pk, sk, blockchain, txn_list, balance}) do
+    {:reply, balance, {pk, sk, blockchain, txn_list, balance}}
   end
 
-  def handle_call(:update_wallet, _from, {pk, sk, blockchain, balance}) do
+  # rewrite to iterate over just latest block
+  def handle_call(:update_wallet, _from, {pk, sk, blockchain, txn_list, balance}) do
     my_name = "node_" <> Mitbits.Utility.getHash(pk)
 
     # IO.inspect blockchain
@@ -52,43 +53,87 @@ defmodule Mitbits.Node do
       end)
 
     # IO.puts pk
-    IO.inspect balance
+    IO.inspect(balance)
 
-    {:reply, {:ok}, {pk, sk, blockchain, balance}}
+    {:reply, {:ok}, {pk, sk, blockchain, txn_list, balance}}
   end
 
-  def handle_call({:buy_bitcoins, miner_node_hash}, _from, {pk, sk, blockchain, balance}) do
-    {:ok} =
-      GenServer.call(
-        Mitbits.Utility.string_to_atom("node_" <> miner_node_hash),
-        {:req_for_mitbits, 10, pk}
-      )
+  def handle_call(
+        {:buy_bitcoins, miner_node_hash},
+        _from,
+        {pk, sk, blockchain, txn_list, balance}
+      ) do
+    GenServer.cast(
+      Mitbits.Utility.string_to_atom("node_" <> miner_node_hash),
+      {:req_for_mitbits, 10, Mitbits.Utility.getHash(pk)}
+    )
 
-    {:reply, {:ok}, {pk, sk, blockchain, balance}}
+    {:reply, {:ok}, {pk, sk, blockchain, txn_list, balance}}
   end
 
-  def handle_call({:req_for_mitbits, amount, req_pk}, _from, {pk, sk, blockchain, balance}) do
+  def handle_cast(
+        {:req_for_mitbits, amount, req_hash},
+        {pk, sk, blockchain, txn_list, balance}
+      ) do
     if(balance > amount) do
-      txn_msg = %{amount: amount, from: "node_"<>Mitbits.Utility.getHash(pk), to: "node_"<>Mitbits.Utility.getHash(req_pk)}
+      txn_msg = %{
+        amount: amount,
+        from: "node_" <> Mitbits.Utility.getHash(pk),
+        to: "node_" <> req_hash
+      }
+
       str_txn_msg = Mitbits.Utility.txn_msg_to_string(txn_msg)
 
       signature_txn_msg = Mitbits.Utility.sign(str_txn_msg, sk)
 
       txn = %{signature: signature_txn_msg, message: txn_msg, timestamp: System.system_time()}
 
-      {:ok} = Mitbits.Utility.add_txn(txn)
+      updated_txn_list = txn_list ++ [txn]
 
-      {:reply, {:ok}, {pk, sk, blockchain, balance - amount}}
+      # Send block to all
+      [{_, all_nodes}] = :ets.lookup(:mitbits, "nodes")
+
+      my_hash = Mitbits.Utility.getHash(pk)
+
+      Enum.each(all_nodes, fn {hash} ->
+        # IO.inspect(hash)
+
+        if(my_hash != hash) do
+
+            GenServer.cast(
+              Mitbits.Utility.string_to_atom("node_" <> req_hash),
+              {:add_txn, txn}
+            )
+        end
+      end)
+
+      {:noreply, {pk, sk, blockchain, updated_txn_list, balance - amount}}
+    else
+      {:noreply, {pk, sk, blockchain, txn_list, balance}}
     end
   end
 
-  def handle_call(:get_prev_block_hash, _from, {pk, sk, blockchain, balance}) do
+  def handle_call(:get_prev_block_hash, _from, {pk, sk, blockchain, txn_list, balance}) do
     latest_block = Enum.at(blockchain, -1)
-    {:reply, {latest_block.hash}, {pk, sk, blockchain, balance}}
+    {:reply, {latest_block.hash}, {pk, sk, blockchain, txn_list, balance}}
   end
 
-  def handle_call({:rec_new_block, new_block}, _from, {pk, sk, blockchain, balance}) do
+  def handle_call({:rec_new_block, new_block}, _from, {pk, sk, blockchain, txn_list, balance}) do
     updated_blockchain = blockchain ++ [new_block]
-    {:reply,{:ok}, {pk, sk, updated_blockchain, balance}}
+    {:reply, {:ok}, {pk, sk, updated_blockchain, txn_list, balance}}
+  end
+
+  def handle_cast({:add_txn, txn}, {pk, sk, blockchain, txn_list, balance}) do
+    updated_txns = txn_list ++ [txn]
+    {:noreply, {pk, sk, blockchain, updated_txns, balance}}
+  end
+
+  def handle_call({:delete_txns, txn}, _from, {pk, sk, blockchain, txn_list, balance}) do
+    updated_txns = txn_list -- txn
+    {:reply, {:ok}, {pk, sk, blockchain, updated_txns, balance}}
+  end
+
+  def handle_call(:get_txns, _from, {pk, sk, blockchain, txn_list, balance}) do
+    {:reply, {txn_list}, {pk, sk, blockchain, txn_list, balance}}
   end
 end
